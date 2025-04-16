bdbecc343b8dd6bd0dd779e762c9ccae63d5e757
NashibJr <nashibjr@gmail.com>
2025-04-16 10:11:36 -0700

    Feat: Illustrated the controlled components

--- src/pages/MultipleInputs.jsx
+++ src/pages/MultipleInputs.jsx
@@ -1,33 +1,12 @@
 import React from "react";
 
 const MultipleInputs = () => {
-  const [state, setState] = React.useState({
-    email: "",
-    password: "",
-    policies: true,
-    gender: "",
-    country: "Kenya",
-    whyUs: "",
-  });
-
-  const handleChange = (event) => {
-    setState((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const onChangePolicies = () => {
-    setState((prevState) => ({ ...prevState, policies: !prevState.policies }));
-  };
-
-  const onChangeGender = (event) => {
-    setState((prevState) => ({ ...prevState, gender: event.target.value }));
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(state, "::::");
+    const formData = new FormData(event.target);
+    const values = formData.entries();
+    const myValues = Object.fromEntries([...values]);
+    console.log(myValues);
   };
 
   return (
@@ -36,63 +15,23 @@ const MultipleInputs = () => {
       onSubmit={handleSubmit}
     >
       <div>
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={state.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={state.password}
-          onChange={handleChange}
-        />
+        <input type="email" placeholder="Email" name="email" />
+        <input type="password" placeholder="Password" name="password" />
       </div>
       <br />
       <label>
-        <input
-          type="checkbox"
-          name="policies"
-          value={state.policies}
-          onChange={onChangePolicies}
-          checked={state.policies}
-        />{" "}
-        Agree to terms and Policies
+        <input type="checkbox" name="policies" /> Agree to terms and Policies
       </label>
       <br />
       <label>
-        Gender: <br />{" "}
-        <input
-          type="radio"
-          name="gender"
-          value={"Male"}
-          onChange={onChangeGender}
-          checked={state.gender === "Male"}
-        />{" "}
-        Male{" "}
-        <input
-          type="radio"
-          name="gender"
-          value={"Female"}
-          onChange={onChangeGender}
-          checked={state.gender === "Female"}
-        />{" "}
-        Female{" "}
-        <input
-          type="radio"
-          name="gender"
-          value={"Prefer not to say"}
-          onChange={onChangeGender}
-          checked={state.gender === "Prefer not to say"}
-        />{" "}
-        Prefer not to say
+        Gender: <br /> <input type="radio" name="gender" value={"Male"} /> Male{" "}
+        <input type="radio" name="gender" value={"Female"} /> Female{" "}
+        <input type="radio" name="gender" value={"Prefer not to say"} /> Prefer
+        not to say
       </label>
       <label>
         Country: <br />
-        <select name="country" value={state.country} onChange={handleChange}>
+        <select name="country">
           <option value={"Uganda"}>Uganda</option>
           <option value={"Kenya"}>Kenya</option>
           <option value={"Tanzania"}>Tanzania</option>
@@ -101,12 +40,7 @@ const MultipleInputs = () => {
       </label>
       <label>
         Why choose us? <br />
-        <textarea
-          rows={5}
-          name="whyUs"
-          value={state.whyUs}
-          onChange={handleChange}
-        ></textarea>
+        <textarea rows={5} name="whyUs"></textarea>
       </label>
       <input type="submit" value={"Submit"} />
     </form>
